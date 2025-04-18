c8c2a65d84a31e9a8a1ddd0aa3928e1e37952465
Logan Teeple <lteeple@olin.edu>
2025-04-18 11:33:16 -0400

    settings.py and model.py

--- model.py
+++ model.py
@@ -1,1 +1,24 @@
+# Cosmic Clash Model]
 
+import pygame
+from settings import HEIGHT, WIDTH, FPS
+
+
+class player:
+    def __init__(self, player_id):
+        if player_id == 1:
+            self.image = pygame.image.load("player1.png")
+            self.x = 10
+        else:
+            self.image = pygame.image.load("player2.png")
+            self.x = WIDTH - 10
+        self.y = HEIGHT / 2
+        self.health = 3
+        self.score = 0
+        self.alive = True
+
+    def move(self, dy):
+        self.y += dy
+
+    def get_position(self):
+        return (self.x, self.y)
