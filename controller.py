e85ffaec7b23080174d3411b9cacf6fb4ee3414b
JaredStivala <jstivala1@babson.edu>
2025-04-25 11:28:56 -0400

    controller

--- controller.py
+++ controller.py
@@ -9,29 +9,33 @@ class Controller:
         self.last_shot_time_p1 = 0
         self.last_shot_time_p2 = 0
 
-    def handle_input(self, events):
-        for event in events:
-            # Player 1 controls
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    self.player1.move(5)  # Move up by 5 pixels
-                elif event.key == pygame.K_s:
-                    self.player1.move(-5)  # Move down by -5 pixels
-                elif event.key == pygame.K_d:
-                    if self._can_shoot(self.last_shot_time_p1):
-                        self.player1.shoot()
-                        self.last_shot_time_p1 = pygame.time.get_ticks()
+        def handle_input(self, events):
+                for event in events:
+                    # Player 1 controls
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_w:
+                            self.player1.dy = -5  # Move up
+                        elif event.key == pygame.K_s:
+                            self.player1.dy = 5  # Move down
+                        elif event.key == pygame.K_d:
+                            self.player1.shoot()
 
-            # Player 2 controls
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    self.player2.move(5)  # Move up by 5 pixels
-                elif event.key == pygame.K_DOWN:
-                    self.player2.move(-5)  # Move down by -5 pixels
-                elif event.key == pygame.K_LEFT:
-                    if self._can_shoot(self.last_shot_time_p2):
-                        self.player2.shoot()
-                        self.last_shot_time_p2 = pygame.time.get_ticks()
+                    elif event.type == pygame.KEYUP:
+                        if event.key in (pygame.K_w, pygame.K_s):
+                            self.player1.dy = 0  # Stop vertical movement
+
+                    # Player 2 controls
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_UP:
+                            self.player2.dy = -5  # Move up
+                        elif event.key == pygame.K_DOWN:
+                            self.player2.dy = 5  # Move down
+                        elif event.key == pygame.K_LEFT:
+                            self.player2.shoot()
+
+                    elif event.type == pygame.KEYUP:
+                        if event.key in (pygame.K_UP, pygame.K_DOWN):
+                            self.player2.dy = 0  # Stop vertical movement
 
     def _can_shoot(self, last_time):
         return pygame.time.get_ticks() - last_time > self.shot_delay
