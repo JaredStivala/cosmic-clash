26384d48fc92183df1090a6d1fcb7cd659754de1
JaredStivala <jstivala1@babson.edu>
2025-04-25 11:13:53 -0400

    controller

--- controller.py
+++ controller.py
@@ -9,28 +9,29 @@ class Controller:
         self.last_shot_time_p1 = 0
         self.last_shot_time_p2 = 0
 
+    def handle_input(self, events):
+        for event in events:
+            # Player 1 controls
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    self.player1.move(5)  # Move up by 5 pixels
+                elif event.key == pygame.K_s:
+                    self.player1.move(-5)  # Move down by -5 pixels
+                elif event.key == pygame.K_d:
+                    if self._can_shoot(self.last_shot_time_p1):
+                        self.player1.shoot()
+                        self.last_shot_time_p1 = pygame.time.get_ticks()
 
-def handle_input(self, keys):
-    # Player 1 controls
-    if keys[pygame.K_w]:
-        self.player1.move_up()
-    if keys[pygame.K_s]:
-        self.player1.move_down()
-    if keys[pygame.K_d]:
-        if self._can_shoot(self.last_shot_time_p1):
-            self.player1.shoot()
-            self.last_shot_time_p1 = pygame.time.get_ticks()
+            # Player 2 controls
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.player2.move(5)  # Move up by 5 pixels
+                elif event.key == pygame.K_DOWN:
+                    self.player2.move(-5)  # Move down by -5 pixels
+                elif event.key == pygame.K_LEFT:
+                    if self._can_shoot(self.last_shot_time_p2):
+                        self.player2.shoot()
+                        self.last_shot_time_p2 = pygame.time.get_ticks()
 
-    # Player 2 Controls
-    if keys[pygame.K_UP]:
-        self.player2.move_up()
-    if keys[pygame.K_DOWN]:
-        self.player2.move_down()
-    if keys[pygame.K_LEFT]:
-        if self._can_shoot(self.last_shot_time_p2):
-            self.player2.shoot()
-            self.last_shot_time_p2 = pygame.time.get_ticks()
-
-
-def _can_shoot(self, last_time):
-    return pygame.time.get_ticks() - last_time > self.shot_delay
+    def _can_shoot(self, last_time):
+        return pygame.time.get_ticks() - last_time > self.shot_delay
